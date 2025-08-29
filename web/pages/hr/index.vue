c3f1bc7b73dfbc64e0410d5d1c8271ba38cd22a8
Mamadou Talla DIA <ibntalla@gmail.com>
2025-08-29 15:50:53 +0000

    TO-17 tarico hero

--- web/pages/hr/index.vue
+++ web/pages/hr/index.vue
@@ -1,3 +1,10 @@
-<script lang="ts" setup>
-await navigateTo(useLocalePath()({ name: "index", replace: true }));
-</script>
+<template>
+  <div style="min-height: 100dvh; padding-top: 80px">
+    <v-container>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quia culpa
+      similique! Optio pariatur dolores, amet accusantium repudiandae expedita
+      excepturi velit architecto nihil assumenda neque cumque, iste praesentium
+      nobis. Maxime?
+    </v-container>
+  </div>
+</template>
