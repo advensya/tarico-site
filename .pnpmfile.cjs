5e01e0a6e1b68cd8ddc368f49f82b4875016f6fd
Mamadou Talla DIA <ibntalla@gmail.com>
2025-10-27 21:15:58 +0000

    11

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -25,6 +25,9 @@ module.exports = {
             if (useToken) {
               // Mode HTTPS avec token
               // deps[depName] = `https://${githubToken}@github.com/${org}/${repoName}.git${ref}`;
+              console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+              console.log(ref);
+
               deps[depName] = `https://${githubToken}@github.com/${org}/${repoName}/tree/${ref.replace('#', '')}`;
 
             } else {
